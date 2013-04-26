a74997c3f4f5e8d03e66b9ae007222a3218fd435
Glenda Gonzalez <glenda-mari20@hotmail.com>
2013-04-26 00:35:39 -0600

    creacion de inicio de secion
    en esta modificacion al sitema se genero el archivo index.php que sera
    donde el usuario inicie secion

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+<style type="text/css">
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+}
+</style>
+</head>
+
+<body>
+<table width="100%" border="0">
+  <tr>
+    <td align="center" bgcolor="#000066"><h1>Inicie secion en el sistema de bypass</h1></td>
+  </tr>
+  <tr>
+    <td align="center" bgcolor="#009900"><form id="form1" name="form1" method="post" action="">
+      <table width="300" border="0">
+        <tr>
+          <td>Usuario</td>
+          <td><label for="textfield2"></label>
+            <input type="text" name="textfield2" id="textfield2" /></td>
+        </tr>
+        <tr>
+          <td>Contraseni</td>
+          <td><label for="textfield"></label>
+            <input type="password" name="textfield" id="textfield" /></td>
+        </tr>
+        <tr>
+          <td>&nbsp;</td>
+          <td><input type="submit" name="button" id="button" value="Enviar" /></td>
+        </tr>
+      </table>
+    </form></td>
+  </tr>
+  <tr>
+    <td bgcolor="#FFFFFF">&nbsp;</td>
+  </tr>
+</table>
+</body>
+</html>
